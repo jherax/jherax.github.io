039db4e029efc32c1d1ecd6166b9248a5d455f72
David Rivera <jherax@gmail.com>
2013-12-22 11:59:14 -0500

    Creates the views structure
    - Added README.md
    - Added views/english.html
    - Added views/spanish.html
    - Added images/ico_alert.png
    - Added images/ico_cancel.png
    - Added images/ico_error.png
    - Added images/ico_info.png
    - Added images/ico_success.png
    - Added javascripts/jherax.js v3.5.2
    - Added javascripts/main.js
    - Added javascripts/polyfills/IE9.min.js
    - Added javascripts/polyfills/html5shiv.min.js
    - Added javascripts/polyfills/selectivizr.js
    - Added stylesheets/jherax.css
    - Updated index.html
    - Updated stylesheets/pygment_trac.css
    - Updated stylesheets/stylesheet.css

---
+++ README.md
@@ -0,0 +1,12 @@
+[Samples Site](http://jherax.github.io/)
+============
+
+This site shows examples and documentation for JSU Library
+* Source:
+* https://github.com/jherax/js-utils
+
+* Spanish documentation
+* http://jherax.github.io/?lang=spanish
+
+* English documentation
+* http://jherax.github.io/?lang=english
--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,45 @@
-console.log('This would be the main JS file.');
+(function ($, jsu, language, undefined) {
+  //Document ready
+  $(function() {
+    var lang = jsu.fnGetQueryToString("lang") || "english";
+    $("#select-language").val(lang).trigger("change");
+  });
+  //Sets the language setting
+  $(document).on("change", "#select-language", function() {
+    language.set(this.value);
+    //location.search = "?lang=" + this.value;
+    $("#main-content").load("views/" + this.value + ".html", function (response, status, xhr) {
+        if (location.hash != "") {
+          location.hash = location.hash + "@";
+          location.hash = location.hash.replace("@", "");
+        }
+        $(".add-top-link").append('<a class="cbox-close" href="#">↑ top</a>');
+        $(".add-methods-link").append('<a class="cbox-close" href="#list-of-methods">↑ list</a>');
+        $(".add-jquery-link").append('<a class="cbox-close" href="#jquery-extensions">↑ list</a>');
+        var canOpen = true;
+        $("[data-mcp]").colorbox({
+          innerWidth: 397, innerHeight: 177,
+          href: "https://www.mcpvirtualbusinesscard.com/VBCServer/jherax/card",
+          iframe: true,
+          fastIframe: true,
+          closeButton: false,
+          overlayClose: false,
+          scrolling: false,
+          arrowKey: false,
+          loop: false,
+          onOpen: function() { canOpen = false; },
+          onLoad: function() { $("#cboxContent").addClass("loader"); $("#circleG").fnCenter({of: "#colorbox"}); },
+          onComplete: function() {
+            $("#cboxLoadedContent iframe").bind("load", function() {
+              $("#cboxContent").removeClass("loader");
+              $("#circleG").appendTo("#loader-iframe");
+            });
+          },
+          onClosed: function() { canOpen = true; }
+        }).on("click", function() { return canOpen; });
+        $('<span class="cbox-close">').text("X").prependTo("#colorbox").on("click", function() {
+          $.colorbox.close();
+        });   
+    });
+  });
+})(jQuery, jsu, window.language);
